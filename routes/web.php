b37e3e5551a89b8ebbaa5d7de59441195251c574
natacio <natacio07@gmailcom>
2021-09-26 16:28:17 -0300

    Login

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,14 @@ Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.de
 Route::get('/posts',[PostController::class, 'index'])->name('posts.index');
 Route::post('/posts',[PostController::class, 'store'])->name('posts.store');
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+require __DIR__.'/auth.php';
