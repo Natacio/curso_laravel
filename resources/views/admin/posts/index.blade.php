1587dd7bcb1ed3a69c693169810e11afee7c735c
natacio <natacio07@gmailcom>
2021-09-26 17:20:13 -0300

    final

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -15,25 +15,54 @@
 @endif
 <form action=" {{ route('posts.search') }} " method="post">
     @csrf
-    <input type="text" name="search" id="search" placeholder="Pesquisar">
+    <input  class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"exit
+     type="text" name="search" id="search" placeholder="Pesquisar">
     <button type="submit">Buscar</button>
 </form>
 
 <h1>Posts</h1>
+<table class="min-w-full divide-y divide-gray-200">
+    <thead class="bg-gray-50">
+        <tr>
+            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Id
+            </th>
+            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Imagem
+            </th>
+            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Nome
+            </th>
+        </th>
+    </thead>
 
-@foreach ($posts as $post )
+    <tbody class="bg-white divide-y divide-gray-200">
+        <tr>
+                @foreach ($posts as $post )
 
-    <p>{{ $post->title }}
-        <img src="{{ url("storage/{$post->image}") }}" alt="{{ $post->title }}" style="max-width:100px;">
+                <td class="px-6 py-4 whitespace-nowrap">
+                    <div class="text-sm text-gray-900">{{ $post->id }}</div>
+                </td>
+                <td>
+                    <img class="h-10 w-10 rounded-full" src="{{ url("storage/{$post->image}") }}" alt="{{ $post->title }}" style="max-width:100px;">
+                </td>
 
-         [
-        <a href="{{ route('posts.show', $post->id ) }}">Ver</a> |
-        <a href="{{ route('posts.edit', $post->id ) }}">Editar</a>
-         ]
+                <td>
+                    <div class="ml-4">
+                    <div class="text-sm text-gray-900">{{ $post->title }}</div>
+                    </div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                    <a class="text-indigo-600 hover:text-indigo-900" href="{{ route('posts.show', $post->id ) }}">Ver</a>
+                </td>
+                <td  class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium" >
+                    <a class="text-indigo-600 hover:text-indigo-900" href="{{ route('posts.edit', $post->id ) }}">Editar</a>
+                </td>
+        </tr>
+                @endforeach
+            </tbody>
+        </table>
 
-    </p>
-
-@endforeach
 
 <hr>
 
