acfc739f3ee0ea1a1bd74786109101d25bca0868
natacio <natacio07@gmailcom>
2021-09-26 17:20:19 -0300

    css

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,14 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::any('/posts/search',[PostController::class, 'search'])->name('posts.search');
-Route::get('/posts/create',[PostController::class, 'create'])->name('posts.create');
-Route::get('posts/{id}',[PostController::class, 'show'])->name('posts.show');
-Route::put('/posts/{id}',[PostController::class, 'update'])->name('posts.update');
-Route::get('/posts/edit/{id}',[PostController::class, 'edit'])->name('posts.edit');
-Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
-Route::get('/posts',[PostController::class, 'index'])->name('posts.index');
-Route::post('/posts',[PostController::class, 'store'])->name('posts.store');
+Route::any('/posts/search',[PostController::class, 'search'])->name('posts.search')->middleware('auth');
+Route::get('/posts/create',[PostController::class, 'create'])->name('posts.create')->middleware('auth');
+Route::get('posts/{id}',[PostController::class, 'show'])->name('posts.show')->middleware('auth');
+Route::put('/posts/{id}',[PostController::class, 'update'])->name('posts.update')->middleware('auth');
+Route::get('/posts/edit/{id}',[PostController::class, 'edit'])->name('posts.edit')->middleware('auth');
+Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy')->middleware('auth');
+Route::get('/posts',[PostController::class, 'index'])->name('posts.index')->middleware('auth');
+Route::post('/posts',[PostController::class, 'store'])->name('posts.store')->middleware('auth');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
